5cec339f89426bb3075e73f36f784d1fb82282a1
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2015-04-02 21:08:31 +0300

    removed x86 build flags that are now in makefile

--- csrc/luajit/build-linux32.sh
+++ csrc/luajit/build-linux32.sh
@@ -1,3 +1,2 @@
-MAKE=make P=linux32 CCOPT_x86="-march=i686 -msse -msse2" \
-	LDFLAGS="-s -static-libgcc -Wl,-rpath,'\$\$ORIGIN'" \
+MAKE=make P=linux32 LDFLAGS="-s -static-libgcc -Wl,-rpath,'\$\$ORIGIN'" \
 	X0=luajit X=luajit-bin D=libluajit.so A=libluajit.a ./build.sh
